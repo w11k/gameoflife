f3dd7cdaf3a5558e0e0cf4929b1f149d9222e466
Heiko Seeberger <heiko.seeberger@googlemail.com>
2010-08-08 19:35:40 +0200

    Added method neighbours to Cell.

--- src/main/scala/Cell.scala
+++ src/main/scala/Cell.scala
@@ -9,6 +9,12 @@ package com.weiglewilczek.gameoflife
 
 case class Cell(x: Int, y: Int) {
 
+  def neighbours: Traversable[Cell] =
+    for {
+      i <- (x - 1 to x + 1)
+      j <- (y - 1 to y + 1) if (i != x) || (j != y)
+    } yield Cell(i, j)
+
   override def toString = position
 
   private lazy val position = "(%s, %s)".format(x, y)
