715deaed5659ea3cea8ef9d18211c97bc1752334
Heiko Seeberger <heiko.seeberger@googlemail.com>
2010-08-09 07:00:41 +0200

    Added class Generation with val-parameter aliveCells.

---
+++ src/main/scala/Generation.scala
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2010 WeigleWilczek.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.weiglewilczek.gameoflife
+
+class Generation(val aliveCells: Set[Cell] = Set.empty) {
+  require(aliveCells != null, "aliveCells must not be null!")
+}
